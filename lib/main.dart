9d02ea501781087d993b74fe40dd120f7ec53c65
Vadim Popov <m.vadimpopov@gmail.com>
2019-07-19 21:29:42 +0300

    1. Changed the main theme of application to dark
    Signed-off-by: Vadim Popov <m.vadimpopov@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'News feed',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.dark(),
       home: const NewsFeed(title: 'News feed'),
     );
   }
